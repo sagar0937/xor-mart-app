f036fe86576d80cc8922ed5e23eda7e016df1a29
sagar0937 <mahindra.sagar@gmail.com>
2020-06-10 13:41:27 +0530

    css chanages

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,15 +6,15 @@ import './Login.css';
 
 
 export const Login = (props) => {
-  const [username, setUsername] = useState('AA');
-  const [password, setPassword] = useState('AAA');
+  const [username, setUsername] = useState('TestUser');
+  const [password, setPassword] = useState('Aa1@@');
   // const [isButtonDisabled, setIsButtonDisabled] = useState(true);
   // const [helperText, setHelperText] = useState('');
   // const [error, setError] = useState(false);
 
   const handleLogin = (e) => {
     e.preventDefault();
-    if (username === 'AA' && password === 'AAA') {
+    if (username === 'TestUser' && password === 'Aa1@@') {
       return props.history.push("/home");
       // return  <Redirect to='/home' />
     } else {
@@ -25,7 +25,7 @@ export const Login = (props) => {
 
 	return (
 		<>
-			  <div className="d-flex justify-content-center h-100">
+			  <div className="d-flex justify-content-center custom-login-wrp">
               <div className="card">
                 <div className="card-header">
                   <h3>Sign In</h3>
@@ -74,106 +74,9 @@ export const Login = (props) => {
                
               </div>
             </div>
-			{/* <form>
-                <h3>Sign In</h3>
-
-                <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
-                </div>
-
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
-                </div>
-
-                <div className="form-group">
-                    <div className="custom-control custom-checkbox">
-                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                    </div>
-                </div>
-
-                <button type="submit" className="btn btn-primary btn-block">Submit</button>
-                <p className="forgot-password text-right">
-                    Forgot <a href="#">password?</a>
-                </p>
-            </form> */}
+	
 		</>
 	)
 }
 
-//  class Login1 extends Component {
-//     render() {
-//         return (
-//           <>
-// 	  <div class="d-flex justify-content-center h-100">
-//               <div class="card">
-//                 <div class="card-header">
-//                   <h3>Sign In</h3>
-//                   <div class="d-flex justify-content-end social_icon">
-//                     <span>
-//                       <i class="fab fa-facebook-square"></i>
-//                     </span>
-//                     <span>
-//                       <i class="fab fa-google-plus-square"></i>
-//                     </span>
-//                     <span>
-//                       <i class="fab fa-twitter-square"></i>
-//                     </span>
-//                   </div>
-//                 </div>
-//                 <div class="card-body">
-//                   <form>
-//                     <div class="input-group form-group">
-//                       <div class="input-group-prepend">
-//                         <span class="input-group-text">
-//                           <i class="fas fa-user"></i>
-//                         </span>
-//                       </div>
-//                       <input
-//                         type="text"
-//                         class="form-control"
-//                         placeholder="username"
-//                       />
-//                     </div>
-//                     <div class="input-group form-group">
-//                       <div class="input-group-prepend">
-//                         <span class="input-group-text">
-//                           <i class="fas fa-key"></i>
-//                         </span>
-//                       </div>
-//                       <input
-//                         type="password"
-//                         class="form-control"
-//                         placeholder="password"
-//                       />
-//                     </div>
-//                     <div class="row align-items-center remember">
-//                       <input type="checkbox" />
-//                       Remember Me
-//                     </div>
-//                     <div class="form-group">
-//                       <input
-//                         type="submit"
-//                         value="Login"
-//                         class="btn float-right login_btn"
-//                       />
-//                     </div>
-//                   </form>
-//                 </div>
-//                 <div class="card-footer">
-//                   <div class="d-flex justify-content-center links">
-//                     Don't have an account?<a href="#">Sign Up</a>
-//                   </div>
-//                   <div class="d-flex justify-content-center">
-//                     <a href="#">Forgot your password?</a>
-//                   </div>
-//                 </div>
-//               </div>
-//             </div>
-//           </>
-//         );
-//     }
-// }
 
